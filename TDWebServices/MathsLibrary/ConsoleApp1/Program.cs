c6242ce1861354a47db9b67641ac567bff240c31
Alexandre HILTCHER <alexandre.hiltcher@etu.unice.fr>
2018-03-22 16:23:44 +0100

    TD 3 + TD ABC

---
+++ TDWebServices/MathsLibrary/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperationsClient client = new MathOperationsClient();
+            Console.WriteLine(client.Add(100, 101));
+            Console.WriteLine(client.Multiply(100, 101));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TDWebServices/MathsLibrary/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.ServiceReference1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MathOperationsClient serviceReference1;
+        MathOperationsClient serviceReference2;
+        public Form1()
+        {
+            serviceReference1 = new MathOperationsClient("math1");
+            serviceReference2 = new MathOperationsClient("math2");
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "" + serviceReference1.Add(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "" + serviceReference2.Multiply(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+        }
+    }
+}
